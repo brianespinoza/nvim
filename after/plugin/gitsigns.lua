89533a698cb25022c0e93f6954ac888412fe1545
Brian Espinoza <briane@uci.edu>
2024-03-10 16:16:57 -0700

    updated gitsigns mappings

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -15,15 +15,17 @@ require('gitsigns').setup{
         map('n', '<leader>gp', gs.preview_hunk)
         map('n', '<leader>gb', function() gs.blame_line{full=true} end)
 
-        map('n', '<leader>hd', gs.diffthis)
-        map('n', '<leader>hD', function() gs.diffthis('~') end)
+        -- diff against staged version
+        map('n', '<leader>fd', gs.diffthis)
+
+        -- diff against last committed version
+        map('n', '<leader>fD', function() gs.diffthis('~') end)
+
         map('n', '<leader>td', gs.toggle_deleted)
 
         map('v', '<leader>gs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
         map('v', '<leader>gr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
 
-        -- Text object
-        map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
     end
 }
 vim.cmd("set statusline+=%{get(b:,'gitsigns_status','')}");
